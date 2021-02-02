6421fbbdc9766fd1d19bc425aba7664e1227c9c5
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-01 20:01:40 -0600

    chore: add colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'var(--font-base)',
+    },
+    extend: {
+      colors: {
+        neutral: 'var(--color-grayish-blue)',
+        primary: 'var(--color-soft-blue)',
+        'primary-dark': 'var(--color-dark-blue)',
+        secondary: 'var(--color-soft-red)',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
