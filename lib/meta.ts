b6727f8f798e57eac826e96454912404d9e19158
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-01 19:43:17 -0600

    feat: add app identifiers

---
+++ lib/meta.ts
@@ -0,0 +1,9 @@
+/**
+ * App title
+ */
+export const title = 'Bookmark';
+
+/**
+ * App description
+ */
+export const description = 'A clean and simple interface to organize your favourite wevsites. Open a new browser tab and see your sites load instantly.';
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,17 @@
+import { title, description } from './lib/meta';
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'bookmark-landing',
+    title,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: description },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -30,23 +32,23 @@ export default {
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
   ],
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: 'en',
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
-}
+  },
+};
