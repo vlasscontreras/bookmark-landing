f63b5f577f4d71912a438da06f804707f4783555
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-02 18:42:37 -0600

    feat: add the social menu

---
+++ components/navigation/SocialNav.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center justify-center">
+    <a href="https://www.facebook.com/bookmark.io" class="px-4 inline-block">
+      <Facebook width="24" class="text-white" />
+    </a>
+    <a href="https://www.twitter.com/bookmark.io" class="px-4 inline-block">
+      <Twitter width="24" class="text-white" />
+    </a>
+  </div>
+</template>
