72231901604bef62b01185e6d97d4d70889657e1
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-03 20:26:55 -0600

    feat: make the hero shape reusable

---
+++ components/InfiniteShape.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="infinite-shape absolute" />
+</template>
+
+<style scoped>
+.infinite-shape {
+  border-radius: 100rem 0 0 100rem;
+  height: 99%;
+  width: 100vw;
+}
+
+@screen md {
+  .infinite-shape {
+    height: 91%;
+  }
+}
+</style>
--- components/sections/HomeHeader.vue
+++ components/sections/HomeHeader.vue
@@ -3,7 +3,7 @@
     <Container class="grid md:grid-cols-2 py-10 xl:py-20 items-center gap-24 md:gap-16 lg:gap-24">
       <!-- Illustration Column -->
       <div class="md:order-2 relative">
-        <div class="hero-shape bg-primary absolute left-1/4 md:left-1/3 top-11 md:top-36" />
+        <InfiniteShape class="bg-primary left-1/4 md:left-1/3 top-11 md:top-36" />
         <HeroGraphic class="relative max-w-full md:max-w-none shadow-2xl rounded-3xl overflow-visible" />
       </div>
 
@@ -31,17 +31,3 @@
     </Container>
   </section>
 </template>
-
-<style scoped>
-.hero-shape {
-  border-radius: 100rem 0 0 100rem;
-  height: 99%;
-  width: 100vw;
-}
-
-@screen md {
-  .hero-shape {
-    height: 91%;
-  }
-}
-</style>
