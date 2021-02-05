fe27c892502a99afe5efe7118483982b9aff089b
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-04 21:49:14 -0600

    chore: use tab components in the features section

--- components/sections/Features.vue
+++ components/sections/Features.vue
@@ -13,35 +13,8 @@
       </div>
 
       <!-- Section Tabs -->
-      <div class="tabs flex justify-center">
-        <button v-for="tab in tabs" :key="`tab-${tab.id}`" :class="{ 'px-4': true, 'focus:outline-none': true, 'text-secondary': activeTab === tab.id }" @click="setActive(tab.id)">
-          {{ tab.title }}
-        </button>
-      </div>
-
-      <div class="content">
-        <div v-for="tab in tabs" :key="`content-${tab.id}`" :class="{hidden: activeTab !== tab.id}">
-          <div class="grid grid-cols-2">
-            <div class="col">
-              Image
-            </div>
-
-            <div class="col">
-              <h3 class="text-primary-dark font-medium text-2xl sm:text-3xl md:text-4xl mb-6">
-                {{ tab.subTitle }}
-              </h3>
-
-              <p class="text-neutral text-lg mb-6">
-                {{ tab.content }}
-              </p>
-
-              <Button :to="tab.cta.url">
-                {{ tab.cta.text }}
-              </Button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <TabCollection :tabs="features" :current="currentTab" @change="setCurrent" />
+      <TabContentCollection :tabs="features" :current="currentTab" :component="featureComponent" />
     </Container>
   </section>
 </template>
@@ -49,48 +22,33 @@
 <script lang="ts">
 import Vue from 'vue';
 
+import FeatureRepository from '@/services/FeatureRepository';
+import FeatureContent from '@/components/Feature.vue';
+import { Feature } from '@/types';
+
 export default Vue.extend({
+  props: {
+    featureRepository: {
+      type: FeatureRepository,
+      default: () => new FeatureRepository(),
+    },
+  },
+
   data () {
     return {
-      activeTab: 'bookmarking',
-      tabs: [
-        {
-          id: 'bookmarking',
-          title: 'Simple Bookmarking',
-          subTitle: 'Bookmark in one click',
-          content: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
-          cta: {
-            text: 'More Info',
-            url: '#',
-          },
-        },
-        {
-          id: 'searching',
-          title: 'Speedy Searching',
-          subTitle: 'Intelligent search',
-          content: 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
-          cta: {
-            text: 'More Info',
-            url: '#',
-          },
-        },
-        {
-          id: 'sharing',
-          title: 'Easy Sharing',
-          subTitle: 'Share your bookmarks',
-          content: 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
-          cta: {
-            text: 'More Info',
-            url: '#',
-          },
-        },
-      ],
+      currentTab: '',
+      features: [] as Array<Feature>,
+      featureComponent: FeatureContent,
     };
   },
 
+  async created () {
+    this.features = await this.featureRepository.all();
+  },
+
   methods: {
-    setActive (id: string) {
-      this.activeTab = id;
+    setCurrent (id: string) {
+      this.currentTab = id;
     },
   },
 });
