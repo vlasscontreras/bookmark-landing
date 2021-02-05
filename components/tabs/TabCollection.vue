6766b89ba7706cd372684c1af9c380381dd7906c
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-04 19:55:35 -0600

    feat: add the tab collection component

---
+++ components/tabs/TabCollection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tabs flex justify-center flex-col md:flex-row max-w-2xl mx-auto mb-12">
+    <Tab v-for="tab in tabs" :key="tab.id" :current="tab.id === current" @click="setCurrent(tab.id)">
+      {{ tab.title }}
+    </Tab>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    tabs: {
+      type: Array,
+      required: true,
+    },
+    current: {
+      type: String,
+      default: null,
+    },
+  },
+
+  methods: {
+    setCurrent (id: String) {
+      this.$emit('change', id);
+    },
+  },
+});
+</script>
