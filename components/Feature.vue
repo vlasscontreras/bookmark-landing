c69815075a266ae483938b4636472c7e91ce33db
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-04 21:48:36 -0600

    feat: add the feature model component

---
+++ components/Feature.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-20">
+    <div class="col relative">
+      <InfiniteShape class="bg-primary right-1/4 md:right-1/3 top-11 md:top-36" direction="left" />
+      <component :is="content.image" class="relative max-w-full" />
+    </div>
+
+    <div class="col">
+      <h3 class="text-primary-dark font-medium text-2xl sm:text-3xl md:text-4xl mb-6">
+        {{ content.title }}
+      </h3>
+
+      <p class="text-neutral text-lg mb-6">
+        {{ content.content }}
+      </p>
+
+      <Button :to="content.path">
+        More Info
+      </Button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import FeatureBookmark from '@/components/illustrations/FeatureBookmark.vue';
+import FeatureSearch from '@/components/illustrations/FeatureSearch.vue';
+import FeatureShare from '@/components/illustrations/FeatureShare.vue';
+
+export default Vue.extend({
+  components: {
+    FeatureBookmark,
+    FeatureSearch,
+    FeatureShare,
+  },
+
+  props: {
+    content: {
+      type: Object,
+      required: true,
+    },
+  },
+});
+</script>
