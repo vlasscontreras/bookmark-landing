2c4294adfb5188746bac055a87e83a515c4c463e
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-04 21:11:47 -0600

    feat: allow multi-direction on the infinite shape component

--- components/InfiniteShape.vue
+++ components/InfiniteShape.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="infinite-shape absolute" />
+  <div :class="{'infinite-shape': true, 'absolute': true, 'shape-left': direction === 'left'}" />
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    direction: {
+      type: String,
+      default: 'right',
+      validator: (value) => {
+        return ['left', 'right'].includes(value);
+      },
+    },
+  },
+});
+</script>
 
 <style scoped>
 .infinite-shape {
-  border-radius: 100rem 0 0 100rem;
   height: 99%;
   width: 100vw;
+}
+
+.infinite-shape:not(.shape-left) {
+  border-radius: 100rem 0 0 100rem;
+}
+
+.infinite-shape.shape-left {
+  border-radius: 0 100rem 100rem 0;
 }
 
 @screen md {
