1d8f4e7451a12b5e7e740a618d8dad954dcf1ff5
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-05 19:42:14 -0600

    fix: feature illustration rendering

--- components/illustrations/FeatureSearch.vue
+++ components/illustrations/FeatureSearch.vue
@@ -1,191 +1,138 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    viewBox="0 0 478 416"
-    width="478"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 478 416" width="478">
     <defs>
       <mask
         id="a"
-        x="-3.53"
+        x="0"
         y="0"
-        width="547"
+        width="350"
         height="346"
         maskUnits="userSpaceOnUse"
       >
         <rect width="350" height="346" rx="8" fill="#fff" />
       </mask>
+      <filter id="shadow">
+        <feDropShadow
+          dx="0"
+          dy="5"
+          std-deviation="5"
+          flood-color="#000000"
+          flood-opacity="0.15"
+        />
+      </filter>
     </defs>
-    <rect width="350" height="346" rx="8" fill="#e8eaf6" />
-    <g mask="url(#a)">
-      <path d="M-3.53.2h547v59h-547Z" fill="#242a45" fill-rule="evenodd" />
-      <circle
-        cx="38.47"
-        cy="30.2"
-        r="16"
+    <g>
+      <g mask="url(#a)">
+        <g>
+          <g>
+            <path d="M0,338a8,8,0,0,0,8,8H342a8,8,0,0,0,8-8V59.2H0Z" fill="#e8eaf6" />
+            <path d="M343.75.2H6.25A8,8,0,0,0,0,8V59.2H350V8A8,8,0,0,0,343.75.2Z" fill="#242a45" />
+          </g>
+          <g>
+            <circle
+              cx="38.47"
+              cy="30.2"
+              r="16"
+              fill="#fff"
+              opacity="0.25"
+              style="isolation: isolate;"
+            />
+            <g>
+              <path d="M72.47,20.2h114a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-114a3,3,0,0,1-3-3h0A3,3,0,0,1,72.47,20.2Z" fill="#fff" fill-rule="evenodd" opacity="0.75" style="isolation: isolate;" />
+              <path d="M72.47,34.2h58a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-58a3,3,0,0,1-3-3h0A3,3,0,0,1,72.47,34.2Z" fill="#fff" fill-rule="evenodd" opacity="0.25" style="isolation: isolate;" />
+            </g>
+          </g>
+        </g>
+      </g>
+      <g>
+        <rect
+          x="27"
+          y="138"
+          width="200"
+          height="255"
+          rx="8"
+          filter="url(#shadow)"
+          fill="#fff"
+        />
+        <g>
+          <path d="M98.68,173H194a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H98.68a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,173Z" fill="#242a45" fill-rule="evenodd" opacity="0.5" style="isolation: isolate;" />
+          <circle cx="66" cy="183" r="16" fill="#5267df" />
+          <path d="M98.68,187h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,187Z" fill="#242a45" fill-rule="evenodd" opacity="0.25" style="isolation: isolate;" />
+          <g>
+            <circle cx="66" cy="239" r="16" fill="#5267df" />
+            <path d="M98.68,229H194a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H98.68a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,229Z" fill="#242a45" fill-rule="evenodd" opacity="0.5" style="isolation: isolate;" />
+            <path d="M98.68,243h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,243Z" fill="#242a45" fill-rule="evenodd" opacity="0.25" style="isolation: isolate;" />
+          </g>
+          <g>
+            <circle cx="66" cy="295" r="16" fill="#5267df" />
+            <path d="M98.68,285H194a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H98.68a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,285Z" fill="#242a45" fill-rule="evenodd" opacity="0.5" style="isolation: isolate;" />
+            <path d="M98.68,299h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,299Z" fill="#242a45" fill-rule="evenodd" opacity="0.25" style="isolation: isolate;" />
+          </g>
+          <g>
+            <circle cx="66" cy="351" r="16" fill="#5267df" />
+            <path d="M98.68,341H194a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H98.68a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,341Z" fill="#242a45" fill-rule="evenodd" opacity="0.5" style="isolation: isolate;" />
+            <path d="M98.68,355h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,355Z" fill="#242a45" fill-rule="evenodd" opacity="0.25" style="isolation: isolate;" />
+          </g>
+        </g>
+      </g>
+      <g>
+        <rect
+          x="251"
+          y="138"
+          width="200"
+          height="255"
+          rx="8"
+          filter="url(#shadow)"
+          fill="#fff"
+        />
+        <g>
+          <path d="M322.68,173H418a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H322.68a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,173Z" fill="#242a45" fill-rule="evenodd" opacity="0.5" style="isolation: isolate;" />
+          <circle cx="290" cy="183" r="16" fill="#5267df" />
+          <path d="M322.68,187h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,187Z" fill="#242a45" fill-rule="evenodd" opacity="0.25" style="isolation: isolate;" />
+          <g>
+            <circle cx="290" cy="239" r="16" fill="#fa5959" />
+            <path d="M322.68,229H418a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H322.68a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,229Z" fill="#242a45" fill-rule="evenodd" opacity="0.5" style="isolation: isolate;" />
+            <path d="M322.68,243h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,243Z" fill="#242a45" fill-rule="evenodd" opacity="0.25" style="isolation: isolate;" />
+          </g>
+          <g>
+            <circle cx="290" cy="295" r="16" fill="#5267df" />
+            <path d="M322.68,285H418a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H322.68a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,285Z" fill="#242a45" fill-rule="evenodd" opacity="0.5" style="isolation: isolate;" />
+            <path d="M322.68,299h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,299Z" fill="#242a45" fill-rule="evenodd" opacity="0.25" style="isolation: isolate;" />
+          </g>
+          <g>
+            <circle cx="290" cy="351" r="16" fill="#5267df" />
+            <path d="M322.68,341H418a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H322.68a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,341Z" fill="#242a45" fill-rule="evenodd" opacity="0.5" style="isolation: isolate;" />
+            <path d="M322.68,355h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,355Z" fill="#242a45" fill-rule="evenodd" opacity="0.25" style="isolation: isolate;" />
+          </g>
+        </g>
+      </g>
+      <rect
+        x="27"
+        y="85"
+        width="237"
+        height="34"
+        rx="8"
         fill="#fff"
-        opacity="0.25"
       />
-      <path
-        d="M72.47,20.2h114a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-114a3,3,0,0,1-3-3h0A3,3,0,0,1,72.47,20.2Z"
-        fill="#fff"
-        fill-rule="evenodd"
-        opacity="0.75"
-      />
-      <path
-        d="M72.47,34.2h58a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-58a3,3,0,0,1-3-3h0A3,3,0,0,1,72.47,34.2Z"
-        fill="#fff"
-        fill-rule="evenodd"
-        opacity="0.25"
-      />
+      <g>
+        <circle
+          cx="372"
+          cy="169"
+          r="67"
+          fill="#495dcf"
+          fill-opacity="0.2"
+          stroke="#5267df"
+          stroke-width="20"
+        />
+        <path
+          d="M419,217l49,49"
+          fill="none"
+          stroke="#5267df"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="20"
+        />
+      </g>
     </g>
-    <rect x="27" y="138" width="200" height="255" rx="8" />
-    <rect
-      x="27"
-      y="138"
-      width="200"
-      height="255"
-      rx="8"
-      fill="#fff"
-    />
-    <path
-      d="M98.68,173H194a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H98.68a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,173Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.5"
-    />
-    <circle cx="66" cy="183" r="16" fill="#5267df" />
-    <path
-      d="M98.68,187h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,187Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.25"
-    />
-    <circle cx="66" cy="239" r="16" fill="#5267df" />
-    <path
-      d="M98.68,229H194a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H98.68a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,229Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.5"
-    />
-    <path
-      d="M98.68,243h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,243Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.25"
-    />
-    <circle cx="66" cy="295" r="16" fill="#5267df" />
-    <path
-      d="M98.68,285H194a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H98.68a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,285Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.5"
-    />
-    <path
-      d="M98.68,299h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,299Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.25"
-    />
-    <circle cx="66" cy="351" r="16" fill="#5267df" />
-    <path
-      d="M98.68,341H194a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H98.68a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,341Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.5"
-    />
-    <path
-      d="M98.68,355h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,98.68,355Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.25"
-    />
-    <rect x="251" y="138" width="200" height="255" rx="8" />
-    <rect
-      x="251"
-      y="138"
-      width="200"
-      height="255"
-      rx="8"
-      fill="#fff"
-    />
-    <path
-      d="M322.68,173H418a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H322.68a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,173Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.5"
-    />
-    <circle cx="290" cy="183" r="16" fill="#5267df" />
-    <path
-      d="M322.68,187h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,187Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.25"
-    />
-    <circle cx="290" cy="239" r="16" fill="#fa5959" />
-    <path
-      d="M322.68,229H418a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H322.68a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,229Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.5"
-    />
-    <path
-      d="M322.68,243h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,243Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.25"
-    />
-    <circle cx="290" cy="295" r="16" fill="#5267df" />
-    <path
-      d="M322.68,285H418a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H322.68a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,285Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.5"
-    />
-    <path
-      d="M322.68,299h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,299Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.25"
-    />
-    <circle cx="290" cy="351" r="16" fill="#5267df" />
-    <path
-      d="M322.68,341H418a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3H322.68a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,341Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.5"
-    />
-    <path
-      d="M322.68,355h48a3,3,0,0,1,3,3h0a3,3,0,0,1-3,3h-48a3,3,0,0,1-3-3h0A3,3,0,0,1,322.68,355Z"
-      fill="#242a45"
-      fill-rule="evenodd"
-      opacity="0.25"
-    />
-    <rect
-      x="27"
-      y="85"
-      width="237"
-      height="34"
-      rx="8"
-      fill="#fff"
-    />
-    <circle
-      cx="372"
-      cy="169"
-      r="67"
-      fill="#495dcf"
-      fill-opacity="0.2"
-      stroke="#5267df"
-      stroke-width="20"
-    />
-    <path
-      d="M419,217l49,49"
-      fill="none"
-      stroke="#5267df"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="20"
-    />
   </svg>
 </template>
