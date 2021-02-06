73a7e038ab21668e2c596fea5c68aeee6114e367
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-05 21:23:43 -0600

    feat: reorganize and add more translations

--- i18n/en.ts
+++ i18n/en.ts
@@ -1,11 +1,65 @@
 export default {
+  /* Uncategorized */
+  addInstallExtension: 'Add & Install Extension',
+  addTo: 'Add to',
   bookmarkLogo: 'Logo of Bookmark',
   closeMenu: 'Close Menu',
   contact: 'Contact',
-  facebookLinkNewTab: 'Give us a Like on Facebook (opens in a new tab)',
   features: 'Features',
+  getItChrome: 'Get it on Chrome',
+  getItFirefox: 'Get it on Firefox',
   login: 'Log In',
+  minVersion: 'Minimum version',
+  moreInfo: 'More Info',
   pricing: 'Pricing',
   toggleMenu: 'Toggle Menu',
-  twitterLinkNewTab: 'Follow us on Twitter (opens in a new tab)',
+
+  /* Categorized */
+  links: {
+    newTab: {
+      facebook: 'Give us a Like on Facebook (opens in a new tab)',
+      twitter: 'Follow us on Twitter (opens in a new tab)',
+    },
+  },
+
+  logos: {
+    facebook: 'Logo of Facebook',
+    twitter: 'Logo of Twitter',
+    googleChrome: 'Logo of Google Chrome',
+    firefox: 'Logo of Firefox',
+    opera: 'Logo of Opera',
+  },
+
+  /* Sections */
+  sections: {
+    hero: {
+      heading: 'A Simple Bookmark Manager',
+      blurb: 'A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.',
+    },
+    features: {
+      heading: 'Features',
+      blurb: 'Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go.',
+      content: {
+        bookmark: {
+          title: 'Simple Bookmarking',
+          heading: 'Bookmark in one click',
+          content: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
+        },
+        search: {
+          title: 'Speedy Searching',
+          heading: 'Intelligent search',
+          content: 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
+        },
+        share: {
+          title: 'Easy Sharing',
+          heading: 'Share your bookmarks',
+          content: 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
+        },
+      },
+    },
+    extension: {
+      heading: 'Download the extension',
+      blurb: 'We\'ve got more browsers in the pipeline. Please do let us know if you\'ve got a favourite you\'d like us to prioritize.',
+    },
+  },
 };
--- i18n/es.ts
+++ i18n/es.ts
@@ -1,11 +1,65 @@
 export default {
+  /* Uncategorized */
+  addInstallExtension: 'Instalar Extensión',
+  addTo: 'Añadir a',
   bookmarkLogo: 'Logo de Bookmark',
   closeMenu: 'Cerrar Menú',
   contact: 'Contacto',
-  facebookLinkNewTab: 'Danos un like en Facebook (se abre en una nueva pestaña)',
   features: 'Características',
+  getItChrome: 'Para Chrome',
+  getItFirefox: 'Para Firefox',
   login: 'Iniciar Sesión',
+  minVersion: 'Versión mínima',
+  moreInfo: 'Más Información',
   pricing: 'Precios',
   toggleMenu: 'Mostrar/Ocultar Menú',
-  twitterLinkNewTab: 'Síguenos en Twitter (se abre en una nueva pestaña)',
+
+  /* Categorized */
+  links: {
+    newTab: {
+      facebook: 'Danos un like en Facebook (se abre en una nueva pestaña)',
+      twitter: 'Síguenos en Twitter (se abre en una nueva pestaña)',
+    },
+  },
+
+  logos: {
+    facebook: 'Logo de Facebook',
+    twitter: 'Logo de Twitter',
+    googleChrome: 'Logo de Google Chrome',
+    firefox: 'Logo de Firefox',
+    opera: 'Logo de Opera',
+  },
+
+  /* Sections */
+  sections: {
+    hero: {
+      heading: 'Un Sencillo Administrador de Marcadores',
+      blurb: 'Una interfaz limpia y sencilla para organizar tus sitios web favoritos. Abre una nueva pestaña en tu navegador y mira cómo sus sitios se cargan al instante. Pruébalo gratis.',
+    },
+    features: {
+      heading: 'Características',
+      blurb: 'Nuestro objetivo es facilitarte el acceso a tus sitios favoritos de forma rápida y sencilla. Tus marcadores se sincronizán entre tus dispositivos para que puedas acceder a ellos sobre la marcha.',
+      content: {
+        bookmark: {
+          title: 'Marcadores Simples',
+          heading: 'Añade marcadores con 1 click',
+          content: 'Organiza tus marcadores como quieras. Nuestra sencilla interfaz de arrastrar y soltar te brinda control total sobre cómo quieres manejar tus sitios favoritos.',
+        },
+        search: {
+          title: 'Búsqueda Rápida',
+          heading: 'Búsqueda inteligente',
+          content: 'Nuestra poderosa búsqueda te ayudará a encontrar sitios guardados instantáneamente. No necesitas revisar todos tus marcadores.',
+        },
+        share: {
+          title: 'Comparte Fácilmente',
+          heading: 'Comparte tus marcadores',
+          content: 'Comparte tus marcadores y colecciones con otros fácilmente. Crea un enlace compartible que puedes enviar con el click de un botón',
+        },
+      },
+    },
+    extension: {
+      heading: 'Descarga la extensión',
+      blurb: 'Tenemos más navegadores en proceso. Haznos saber si tienes un favorito que te gustaría que prioricemos!',
+    },
+  },
 };
