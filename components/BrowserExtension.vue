81cc8df5f327fca34337c1742a4b2d3ca6a6f15b
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-02-05 20:22:13 -0600

    feat: add the browser extension box component

---
+++ components/BrowserExtension.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-white shadow-lg p-6 rounded-xl text-center">
+    <component :is="icon" class="mx-auto mb-6" />
+
+    <h3 class="text-primary-dark font-medium text-xl mb-2">
+      Add to {{ name }}
+    </h3>
+    <div class="text-gray-400 text-sm mb-6">
+      Minimum version {{ minVersion }}
+    </div>
+
+    <Dots class="-mx-6 mb-6" :width="false" />
+
+    <Button class="w-full">
+      Add & Install Extension
+    </Button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    minVersion: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      required: true,
+      validator: _ => true,
+    },
+  },
+});
+</script>
